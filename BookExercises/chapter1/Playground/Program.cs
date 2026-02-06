d3549f2cc8bd81109acba419d2e1fe4be890d124
shokooofaaaa <tgshokooofa@gmail.com>
2026-02-06 15:18:32 +0330

    Exercises of chapter1

---
+++ BookExercises/chapter1/Playground/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Banking App");
+Console.WriteLine("Enter your name: ");
+
+string name = Console.ReadLine();
+if (name == null)
+{
+    Console.WriteLine("Name cannot be null");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}! welcome");
+}
+
---
+++ BookExercises/chapter1/balance/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your balance: ");
+string YourBalance = Console.ReadLine();
+
+int.TryParse(YourBalance, out int balance);
+Console.WriteLine($"Your balance is: {balance}");
